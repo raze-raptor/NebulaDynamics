7b098bcd0599c5f4d558a5d54df557c9061dc596
Matthew <50781883+raze-raptor@users.noreply.github.com>
2023-11-20 00:12:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 // Check for the secret command in the console
 function checkConsoleCommand() {
-  console.log("Psst... Try typing 'hello' in the console!");
+  console.log("Psst... Try typing 'hello()' in the console!");
   window.addEventListener('message', function (e) {
-    if (e.data === 'hello') {
-      playIframe();
+    if (e.data === 'hello()') {
+      redirectToSecretPage();
     }
   });
 }
 
-// Function to play the iframe
-function playIframe() {
-  var iframe = document.querySelector('iframe');
-  if (iframe) {
-    iframe.src = "https://d1tm91r4ytbt54.cloudfront.net/4a2bd50a-0f2f-45be-9e66-a2adf5849945/1680626121644/1v1-lol/index.html";
-  }
+// Function to redirect to the secret.html page
+function redirectToSecretPage() {
+  window.location.href = "secret.html";
 }
 
 // Call the function to check the console command
