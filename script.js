3f6016bb23255c5a357b09a2825a4fa6b57c1d80
Matthew <50781883+raze-raptor@users.noreply.github.com>
2023-11-23 23:25:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ function redirectToSecretPage() {
 
 // Call the function to check the console command
 checkConsoleCommand();
+
+// Secret
+let konamiCode = [];
+const secretCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight'];
+
+document.addEventListener('keydown', (event) => {
+  konamiCode.push(event.key);
+  konamiCode = konamiCode.slice(-secretCode.length);
+
+  if (JSON.stringify(konamiCode) === JSON.stringify(secretCode)) {
+    // Activate the easter egg
+    window.location.href = 'secret.html';
+  }
+});
